691c0c62422af8852c52d5eaa3fc9f3e034f1e8f
hemanth2k7-commits <hemanthkumar2k7@gmail.com>
2026-03-20 13:05:47 +0530

    Fixed bugs in calculator

--- Node.JS/Basics/Calc.js
+++ Node.JS/Basics/Calc.js
@@ -1,22 +1,11 @@
 //Basic CLI calculator
-const add = (a, b) =>
-  !isNaN(a) && !isNaN(b)
-    ? console.log(`${a} + ${b} = ${a + b}`)
-    : console.log("Error:Two numbers required");
-const sub = (a, b) =>
-  !isNaN(a) && !isNaN(b)
-    ? console.log(`${a} - ${b} = ${a - b}`)
-    : console.log("Error:Two numbers required");
+const add = (a, b) => console.log(`${a} + ${b} = ${a + b}`);
+const sub = (a, b) => console.log(`${a} - ${b} = ${a - b}`);
 const div = (a, b) =>
-  !isNaN(a) && !isNaN(b)
-    ? b != 0
-      ? console.log(`${a} / ${b} = ${a / b}`)
-      : console.log("Error:Division by zero")
-    : console.log("Error:Two numbers required");
-const mul = (a, b) =>
-  !isNaN(a) && !isNaN(b)
-    ? console.log(`${a} * ${b} = ${a * b}`)
-    : console.log("Error:Two numbers required");
+  b != 0
+    ? console.log(`${a} / ${b} = ${a / b}`)
+    : console.log("Error:Division by zero");
+const mul = (a, b) => console.log(`${a} * ${b} = ${a * b}`);
 
 const readline = require("readline"); //Importing Module for I/O
 const rl = readline.createInterface({
@@ -28,24 +17,29 @@ rl.question("Enter First number:", (num1) => {
   let a = Number(num1);
   rl.question("Enter Second number:", (num2) => {
     let b = Number(num2);
-    rl.question("Choose Operation(add/sub/mul/div):", (op) => {
-      switch (op.toLowerCase()) {
-        case "add":
-          add(a, b);
-          break;
-        case "sub":
-          sub(a, b);
-          break;
-        case "mul":
-          mul(a, b);
-          break;
-        case "div":
-          div(a, b);
-          break;
-        default:
-          console.log("Invalid Operation");
-      }
-      rl.close(); //To close question
-    });
+    if (!isNaN(a) && !isNaN(b)) {
+      rl.question("Choose Operation(add/sub/mul/div):", (op) => {
+        switch (op.toLowerCase()) {
+          case "add":
+            add(a, b);
+            break;
+          case "sub":
+            sub(a, b);
+            break;
+          case "mul":
+            mul(a, b);
+            break;
+          case "div":
+            div(a, b);
+            break;
+          default:
+            console.log("Invalid Operation");
+        }
+        rl.close(); //To close question
+      });
+    } else {
+      console.log("Error:Invalid type(expected a number)");
+      rl.close();
+    }
   });
 });
