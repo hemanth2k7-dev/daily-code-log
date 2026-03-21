bb5246aafe07440b7861a96de21f330f8ef6dacd
hemanth2k7-dev <hemanthkumar2k7@gmail.com>
2026-03-21 19:25:56 +0530

    Converted to proper async/await handled

--- Node.JS/Basics/Calc.js
+++ Node.JS/Basics/Calc.js
@@ -8,38 +8,44 @@ const div = (a, b) =>
 const mul = (a, b) => console.log(`${a} * ${b} = ${a * b}`);
 
 const readline = require("readline"); //Importing Module for I/O
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-}); //Creates an interface of I/O
-rl.question("Enter First number:", (num1) => {
-  //To ask a question in command line and use callback accordingly
-  let a = Number(num1);
-  rl.question("Enter Second number:", (num2) => {
-    let b = Number(num2);
-    if (!isNaN(a) && !isNaN(b)) {
-      rl.question("Choose Operation(add/sub/mul/div):", (op) => {
-        switch (op.toLowerCase()) {
-          case "add":
-            add(a, b);
-            break;
-          case "sub":
-            sub(a, b);
-            break;
-          case "mul":
-            mul(a, b);
-            break;
-          case "div":
-            div(a, b);
-            break;
-          default:
-            console.log("Invalid Operation");
-        }
-        rl.close(); //To close question
-      });
-    } else {
-      console.log("Error:Invalid type(expected a number)");
-      rl.close();
-    }
+
+function askQuestion(query) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  }); //Creates an interface for I/O
+  return new Promise((resolve) => {
+    rl.question(query, (answer) => {
+      //To ask a question in command line and use callback accordingly
+      rl.close(); //To close question
+      resolve(answer);
+    });
   });
-});
+}
+
+async function main() {
+  let a = Number(await askQuestion("Enter first number:"));
+  let b = Number(await askQuestion("Enter second number:"));
+  if (!isNaN(a) && !isNaN(b)) {
+    let op = await askQuestion("Choose Operation(add/sub/mul/div):");
+    switch (op.toLowerCase()) {
+      case "add":
+        add(a, b);
+        break;
+      case "sub":
+        sub(a, b);
+        break;
+      case "mul":
+        mul(a, b);
+        break;
+      case "div":
+        div(a, b);
+        break;
+      default:
+        console.log("Invalid Operation");
+    }
+  } else {
+    console.log("Error:Invalid type(expected a number)");
+  }
+}
+main();
